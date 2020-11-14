d95540e1c626c5d1e31ba5754f78e289925a7752
MinG822 <lucida822@naver.com>
2020-11-14 22:50:46 +0900

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './abc.jpg'
+import Data from './data.xml'
+import Notes from './data.csv'
+import tomlData from './data.toml'
+import yamlData from './data.yaml'
+import jsonData from './data.json5'
+
+console.log(tomlData.title)
+console.log(tomlData.owner.name)
+
+console.log(yamlData.title)
+console.log(yamlData.owner.name)
+
+console.log(jsonData.title)
+console.log(jsonData.owner.name)
 
 function component() {
   const element = document.createElement('div')
+  
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
+
+  const myIcon = new Image()
+  myIcon.src = Icon
+
+  element.appendChild(myIcon)
+
+  console.log(Data)
+  console.log(Notes)
+
   return element
 }
 
